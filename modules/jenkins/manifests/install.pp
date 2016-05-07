8978b64be78b94ad96fe916aa0634e43df527bbb
Lalith <lchennup@gmail.com>
2016-05-06 21:38:19 -0700

    Added updated files

--- modules/jenkins/manifests/install.pp
+++ modules/jenkins/manifests/install.pp
@@ -5,4 +5,24 @@
    $tomcat_version = hiera('tomcat_version')
 
    notify{$tomcat_version:}
+
+   file {"/apps":
+      ensure => "directory",
+
+    }
+
+    group { "appgroup":
+        ensure => present,
+        gid => 2000
+     }->
+     user { "appuser":
+         ensure => present,
+         gid => "appgroup",
+         groups => ["appgroup", "root"],
+         membership => minimum,
+         shell => "/bin/bash",
+     }->
+     exec { "wget  --output-document=/apps/  ": }
+
+   
 }
